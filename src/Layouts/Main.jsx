915b17bc9484bf3ad977e23a62b39281a1f6f02c
md muhaiminul <muhai4959@gmail.com>
2023-06-07 13:23:46 +0600

    added top parallax slider

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavBar from '../Pages/Shared/NavBar';
+import SliderSection from '../Pages/Home/SliderSection';
 
 const Main = () => {
     return (
-        <div>
+        <div className='md:px-28 px-5 space-y-10'>
             <NavBar/>
+            <SliderSection/>
         </div>
     );
 };
---
+++ src/Pages/Home/SliderSection.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "./SliderSection.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, Navigation, Parallax } from "swiper";
+
+const SliderSection = () => {
+	const swiperData = [
+		{
+			text: "Yoga for Beginners",
+			information:
+				"Learn the basics of yoga and start your journey towards a healthier body and mind.",
+			message:
+				"Discover the benefits of yoga and improve your flexibility and overall well-being.",
+		},
+		{
+			text: "Meditation Techniques",
+			information:
+				"Explore different meditation techniques and find inner peace and tranquility.",
+			message:
+				"Learn how to calm your mind, reduce stress, and improve focus through meditation.",
+		},
+		{
+			text: "Yoga Retreat",
+			information:
+				"Join us for a rejuvenating yoga retreat in a serene and picturesque location.",
+			message:
+				"Immerse yourself in yoga, meditation, and connect with like-minded individuals.",
+			picture:
+				"https://www.business-opportunities.biz/wp-content/uploads/2014/12/yoga.jpg",
+		},
+		{
+			text: "Mindfulness Meditation",
+			information:
+				"Practice mindfulness meditation and cultivate a deep sense of awareness in the present moment.",
+			message:
+				"Experience greater clarity, emotional well-being, and enhanced self-awareness.",
+		},
+		{
+			text: "Yoga for Stress Relief",
+			information:
+				"Relieve stress and anxiety with specialized yoga sequences designed to relax your body and mind.",
+			message:
+				"Find inner calmness, improve sleep, and restore balance in your daily life.",
+		},
+	];
+
+	return (
+		<div className="">
+			<Swiper
+				style={{
+					"--swiper-navigation-color": "#fff",
+					"--swiper-pagination-color": "#fff",
+				}}
+                autoplay={{
+                    delay: 4000,
+                    disableOnInteraction: false,
+                  }}
+				speed={600}
+				parallax={true}
+				pagination={{
+					clickable: true,
+				}}
+				navigation={true}
+				modules={[Parallax, Pagination, Navigation, Autoplay]}
+				className="mySwiper"
+			>
+				<div
+					slot="container-start"
+					className="parallax-bg"
+					style={{
+						"background-image":
+							"url(https://i.ibb.co/FBHvNgX/Screenshot-2023-06-07-124036.png)",
+					}}
+					data-swiper-parallax="-23%"
+				></div>
+				{swiperData.map((item) => (
+					<SwiperSlide key={item.text}>
+						<div className="lg:px-28 py-10 h-[500px] space-y-5 flex flex-col justify-center">
+							<h1 className="text-4xl font-semibold text-gray-700">{item.text}</h1>
+							<p className="border-l-4 border-yellow-300 ps-2">{item.information}</p>
+							<blockquote>{item.message}</blockquote>
+						</div>
+					</SwiperSlide>
+				))}
+			</Swiper>
+		</div>
+	);
+};
+
+export default SliderSection;
